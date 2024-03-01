a159e8f03a5facf6cb49124fc84197ccca0c8e1b
takagi-takagi <125945940+takagi-takagi@users.noreply.github.com>
2024-03-01 11:17:43 +0900

    User,Project,Textのリレーションを追加

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    public function texts() {
+        return $this->hasMany(Text::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Text.php
+++ app/Models/Text.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Text extends Model
 {
     use HasFactory;
+
+    public function project() {
+        return $this->belongsTo(Project::class);
+    }
 }

6f45795dee9ab518ba27b8747daa61849686c07f
takagi-takagi <125945940+takagi-takagi@users.noreply.github.com>
2024-03-01 11:11:05 +0900

    モデルの名前の先頭を大文字に変更

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class text extends Model
+class Project extends Model
 {
     use HasFactory;
 }

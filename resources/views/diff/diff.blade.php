48eaacb7ee6beca3a842329c1f68bf87a7801ce7
takagi-takagi <125945940+takagi-takagi@users.noreply.github.com>
2024-03-13 14:10:04 +0900

    ビューの不要な空白を削除

--- resources/views/diff/diff.blade.php
+++ resources/views/diff/diff.blade.php
@@ -26,21 +26,10 @@
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                     <div class="max-w-xl">
                         <p>ID:{{$text->id}}</p>
-                        <p>{!! nl2br(e($text->body)) !!}</p>
+                        {!! nl2br(e($text->body)) !!}
                     </div>
                 </div>
             @endforeach
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    <!-- @include('profile.partials.update-password-form') -->
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    <!-- @include('profile.partials.delete-user-form') -->
-                </div>
-            </div>
         </div>
     </div>
 </x-app-layout>
--- resources/views/diff/indexProject.blade.php
+++ resources/views/diff/indexProject.blade.php
@@ -25,17 +25,6 @@
                     </div>
                 </div>
             @endforeach
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    <!-- @include('profile.partials.update-password-form') -->
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    <!-- @include('profile.partials.delete-user-form') -->
-                </div>
-            </div>
         </div>
     </div>
 </x-app-layout>

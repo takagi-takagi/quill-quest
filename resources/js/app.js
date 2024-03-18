b832b91631f60a471dfb86269ecf7c9eaff42c1b
takagi-takagi <125945940+takagi-takagi@users.noreply.github.com>
2024-03-18 15:18:09 +0900

    コピペボタンを追加

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,14 +14,40 @@ function toggleContent(container) {
     const initialText = container.querySelector('.initial-text');
     const content = container.querySelector('.content');
     const button = container.querySelector('.toggle-button');
+    const buttonContainer = container.parentElement.querySelector('.button-container');
 
     if (initialText.classList.contains('hidden')) {
         content.classList.add('hidden');
         initialText.classList.remove('hidden');
         button.classList.add('rotate-180');
+        buttonContainer.classList.add('hidden');
     } else {
         initialText.classList.add('hidden');
         content.classList.remove('hidden');
         button.classList.remove('rotate-180');
+        buttonContainer.classList.remove('hidden');
     }
 }
+
+document.querySelectorAll('.copyButton').forEach(button => {
+    let timerId;
+    button.addEventListener('click', function() {
+        var parent = this.parentElement;
+
+        var textToCopy = parent.querySelector('.content').textContent;
+
+        // テキストをクリップボードにコピー
+        navigator.clipboard.writeText(textToCopy).then(() => {
+            // コピー成功時の処理
+            this.innerText = 'コピーしました！'; // ボタンのテキストを更新
+        }).catch(err => {
+            // コピー失敗時の処理 
+            this.innerText = 'コピーに失敗しました';
+        });
+        clearTimeout(timerId);
+        // 一定時間後にボタンのテキストを元に戻す
+        timerId = setTimeout(() => {
+            this.innerText ='コピーする';
+        }, 2000); 
+    });
+});
--- resources/views/diff/diff.blade.php
+++ resources/views/diff/diff.blade.php
@@ -59,6 +59,9 @@
                             <span class="initial-text overflow-hidden">{{$text->bodyHead()}}</span>
                             <span class="hidden content">{!! nl2br(e($text->body)) !!}</span>
                         </div>
+                        <div class="button-container hidden">
+                            <button class="copyButton inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">コピーする</button>
+                            </div>
                     </div>
                 </div>
             @endforeach

e5958b0796089180497129a4eec8ff4a46626311
takagi-takagi <125945940+takagi-takagi@users.noreply.github.com>
2024-05-31 13:12:24 +0900

    新規登録をユーザー登録に変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,7 +34,7 @@
                         <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">ログイン</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">新規登録</a>
+                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">ユーザー登録</a>
                         @endif
                     @endauth
                 </div>
